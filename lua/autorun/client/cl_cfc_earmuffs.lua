13116760b0ea47768c5ee7247a3eac6965c11802
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-08-29 11:30:59 -0700

    Control the volume instead of muting

--- lua/autorun/client/cl_cfc_earmuffs.lua
+++ lua/autorun/client/cl_cfc_earmuffs.lua
@@ -4,6 +4,8 @@ local isCombatSound = {
 
 local hookName = "CFC_CombatEarmuffs"
 
+local combatSoundVolumeMult = 0.2
+
 -- TODO: Store this result in a var and use pvp change hooks
 local function isInPvp()
     return LocalPlayer():GetNWBool( "CFC_PvP_Mode", false )
@@ -13,8 +15,29 @@ local function shouldPlayCombatSound( soundData )
     local soundName = soundData.SoundName
     if not isCombatSound[soundName] then return end
 
-    if isInPvp() then return false end
+    if isInPvp() then
+        local volume = soundData.Volume
+        soundData.Volume = volume * combatSoundVolumeMult
+
+        return true
+    end
 end
 
 hook.Remove( "EntityEmitSound", hookName )
 hook.Add( "EntityEmitSound", hookName, shouldPlayCombatSound )
+
+hook.Remove( "PopulateToolMenu", "CFC_CombatVolumeMenu" )
+hook.Add( "PopulateToolMenu", "CFC_CombatVolumeMenu", function()
+    spawnmenu.AddToolMenuOption( "Utilities", "CFC", "Sound Control", "Sound Control", "", "", function( panel )
+        panel:ClearControls()
+
+        local CombatSoundSlider = vgui.Create( "DNumSlider", panel )
+        CombatSoundSlider:SetText( "Combat Sound Volume" )
+        CombatSoundSlider:Dock( TOP )
+        CombatSoundSlider:SetMinMax( 0, 100 )
+        CombatSoundSlider:SetDecimals( 0 )
+        CombatSoundSlider.OnValueChanged = function( newValue )
+            combatSoundVolumeMult = newValue == 0 and 0 or newValue / 100
+        end
+    end )
+end )
