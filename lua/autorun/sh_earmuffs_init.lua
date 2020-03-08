f427d3e8be4c7191ec492552dff39983b931a662
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-03-08 07:58:58 -0400

    Refactor init

---
+++ lua/autorun/sh_earmuffs_init.lua
@@ -0,0 +1,31 @@
+CFCEarmuffs = CFCEarmuffs or {}
+
+if SERVER then
+    AddCSLuaFile( "earmuffs/shared/sh_logger.lua" )
+    AddCSLuaFile( "earmuffs/shared/sh_utils.lua" )
+    AddCSLuaFile( "earmuffs/shared/sh_config.lua" )
+
+    AddCSLuaFile( "earmuffs/client/cl_settings.lua" )
+    AddCSLuaFile( "earmuffs/client/cl_networked_sounds.lua" )
+    AddCSLuaFile( "earmuffs/client/cl_combat_sounds.lua" )
+    AddCSLuaFile( "earmuffs/client/cl_sweps.lua" )
+end
+
+include( "earmuffs/shared/sh_logger.lua" )
+include( "earmuffs/shared/sh_utils.lua" )
+include( "earmuffs/shared/sh_config.lua" )
+
+if SERVER then
+    include( "earmuffs/server/sv_sound_throttler.lua" )
+    include( "earmuffs/server/sv_weapon_ext.lua" )
+    include( "earmuffs/server/sv_hl2_sweps.lua" )
+    include( "earmuffs/server/sv_hl2_npcs.lua" )
+    include( "earmuffs/server/sv_hl2_misc.lua" )
+end
+
+if CLIENT then
+    include( "earmuffs/client/cl_settings.lua" )
+    include( "earmuffs/client/cl_networked_sounds.lua" )
+    include( "earmuffs/client/cl_combat_sounds.lua" )
+    include( "earmuffs/client/cl_sweps.lua" )
+end
