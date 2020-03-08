b50aaca560c7037b39dd344446160b60d6f86266
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-03-08 11:04:56 -0400

    Add default bulletimpact sound. Wire up hl2 misc

--- lua/earmuffs/server/sv_hl2_misc.lua
+++ lua/earmuffs/server/sv_hl2_misc.lua
@@ -5,3 +5,19 @@ local hl2CombatSounds = {
     ["BaseCombatCharacter.AmmoPickup"] = true,
     ["BaseCombatCharacter.StopWeaponSounds"] = true,
 }
+
+local utils = CFCEarmuffs.Utils
+
+local function isHL2Misc( soundData )
+    local soundName = soundData.OriginalSoundName
+
+    if hl2CombatSounds[soundName] then return true end
+end
+
+local function handleHL2MiscSound( soundData )
+    if not isHL2Misc( soundData ) then return end
+
+    return utils.broadcastEntityEmitSound( soundData )
+end
+
+hook.Add( "EntityEmitSound", "CFC_Earmuffs_OnHL2MiscSound", handleHL2MiscSount )
