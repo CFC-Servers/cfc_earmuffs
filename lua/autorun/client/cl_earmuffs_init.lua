f8cae383e23dd58010317c251d405b55d67c14a4
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-03-08 07:58:58 -0400

    Refactor init

--- lua/autorun/client/cl_earmuffs_init.lua
+++
@@ -1,9 +0,0 @@
-CFCEarmuffs = CFCEarmuffs or {}
-
-include( "earmuffs/shared/sh_logger.lua" )
-include( "earmuffs/shared/sh_utils.lua" )
-
-include( "earmuffs/client/cl_settings.lua" )
-include( "earmuffs/client/cl_networked_sounds.lua" )
-include( "earmuffs/client/cl_combat_sounds.lua" )
-include( "earmuffs/client/cl_sweps.lua" )
