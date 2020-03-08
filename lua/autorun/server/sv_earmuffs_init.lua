92deded67600162ca10594096a3abe03256b17ba
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-03-08 07:38:26 -0400

    Fix AddCSLuafiles

--- lua/autorun/server/sv_earmuffs_init.lua
+++ lua/autorun/server/sv_earmuffs_init.lua
@@ -8,3 +8,9 @@ include( "earmuffs/server/sv_weapon_ext.lua" )
 include( "earmuffs/server/sv_hl2_sweps.lua" )
 include( "earmuffs/server/sv_hl2_npcs.lua" )
 include( "earmuffs/server/sv_hl2_misc.lua" )
+
+
+AddCSLuaFile( "earmuffs/client/cl_settings.lua" )
+AddCSLuaFile( "earmuffs/client/cl_networked_sounds.lua" )
+AddCSLuaFile( "earmuffs/client/cl_combat_sounds.lua" )
+AddCSLuaFile( "earmuffs/client/cl_sweps.lua" )
