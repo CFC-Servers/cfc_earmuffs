0ca0fa5b3a5ce97d1689cc0550b658c28bf43a59
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-03-20 22:57:39 -0400

    Pas -> PAS

--- lua/earmuffs/shared/sh_utils.lua
+++ lua/earmuffs/shared/sh_utils.lua
@@ -103,7 +103,7 @@ if SERVER then
 
         local soundTriggerer = findSoundTriggerer( soundData )
         local recipientFilter = RecipientFilter()
-        recipientFilter:AddPas( soundPos )
+        recipientFilter:AddPAS( soundPos )
 
         if soundTrigger then
             recipientFilter:RemovePlayer( soundTriggerer )
